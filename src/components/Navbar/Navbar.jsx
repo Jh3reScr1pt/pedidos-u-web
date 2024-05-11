3671ed3bfda8ad8a126b78d2caeb525adcd84e7c
Jh3reScr1pt <jherescript.404@gmail.com>
2024-05-11 00:40:40 -0400

    Complete frontend

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,50 @@
-import React, { useState } from "react";
-import "./Navbar.css";
-import { assets } from "../../assets/assets";
+import React, { useContext, useEffect, useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import { Link, useNavigate } from 'react-router-dom'
+import { StoreContext } from '../../Context/StoreContext'
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
 
-    const [menu,setMenu] = useState("home");
+  const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount, token ,setToken } = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate('/')
+  }
 
   return (
-    <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+    <div className='navbar'>
+      <Link to='/'><img className='logo' src={assets.logo} alt="" /></Link>
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+        <Link to="/" onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""}`}>home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={`${menu === "menu" ? "active" : ""}`}>menu</a>
+        <a href='#app-download' onClick={() => setMenu("mob-app")} className={`${menu === "mob-app" ? "active" : ""}`}>mobile app</a>
+        <a href='#footer' onClick={() => setMenu("contact")} className={`${menu === "contact" ? "active" : ""}`}>contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
-        </div>
-        <button> sign in</button>
+        <Link to='/cart' className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt="" />
+          <div className={getTotalCartAmount() > 0 ? "dot" : ""}></div>
+        </Link>
+        {!token ? <button onClick={() => setShowLogin(true)}>sign in</button>
+          : <div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="" />
+            <ul className='navbar-profile-dropdown'>
+              <li onClick={()=>navigate('/myorders')}> <img src={assets.bag_icon} alt="" /> <p>Orders</p></li>
+              <hr />
+              <li onClick={logout}> <img src={assets.logout_icon} alt="" /> <p>Logout</p></li> 
+            </ul>
+          </div>
+        }
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
